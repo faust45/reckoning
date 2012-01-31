bbb19ab9825856344886d0d3be30f20d7b4663a4
faust45 <faust451@gmail.com>
2012-02-01 00:55:18 +0200

    Add js

---
+++ _attachments/js/app.js
@@ -0,0 +1,7 @@
+$(function() {
+    //So when document ready we run some code ;)
+    $('#cool-button').click(function() {
+      var date = new Date();
+      alert("Time is: " + date);
+    });
+});
