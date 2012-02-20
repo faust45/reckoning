c043a43e4768b25308606f7e927dd6cbda94fb38
aleks <aleks@gobsec.(none)>
2012-02-20 10:15:31 +0200

    comment

---
+++ _attachments/js/menusketch.js
@@ -0,0 +1,74 @@
+
+
+
+#topbar td.empty { cursor: auto;}
+
+
+<style>
+			body {
+
+
+				margin: 0;
+				overflow: hidden;
+
+				background-color: #ffffff;
+
+				font-family: arial, sans-serif;
+
+			}
+
+			#topbar {
+
+				width: 100%;
+
+				color: #CCC;
+
+				font-size: 19px;
+
+				border-spacing: 0px;
+
+				white-space: nowrap;
+
+			}
+
+
+				#topbar td {
+
+					height: 18px;
+
+					margin: 0px;
+
+					background-color: #2d2d2d;
+
+					border-top: 2px solid transparent;
+
+					white-space: nowrap;
+
+					cursor: pointer;
+				}
+					#topbar td:hover { background-color: #4c4c4c; }
+
+					#topbar td.selected {
+
+						font-weight: bold;
+
+						color: #FFF;
+
+					}
+
+
+  
+                                     
+
+					#topbar td.empty:hover { background-color: #2d2d2d;}
+				
+
+
+
+
+
+
+
+
+			
+		</style>
